6e23c3a3bf96fd920b34fbcad153edb374605d2f
Prokhor Pastushenko <p.pastushenko@calltravel.eu>
2021-02-27 15:10:00 +0200

    add empty docstring

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,8 @@
+"""PLACE FOR DOCSTRING."""
 import prompt
 
 
 def welcome_user():
+    """PLACE FOR DOCSTRING."""
     name = prompt.string('May I have your name? ')
     print('Hello, {name}!'.format(name=name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+"""PLACE FOR DOCSTRING."""
 from brain_games.cli import welcome_user
 
 
 def main():
+    """PLACE FOR DOCSTRING."""
     print('Welcome to the Brain Games!')
     welcome_user()
 
