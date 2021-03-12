751a05eaca4fccf3009574959dcde27121619737
Prokhor Pastushenko <p.pastushenko@calltravel.eu>
2021-03-12 22:09:12 +0200

    update engine logic; delete useless "ask_user" function

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,17 +1,5 @@
 """Game engine."""
 import prompt
-from brain_games import constants
-
-
-def ask_user():
-    """Ask user to provide answer.
-
-    Returns:
-        str
-    """
-    return prompt.string(
-        '{text}: '.format(text=constants.PROMPT_FOR_ANSWER),
-    )
 
 
 def play(game):
@@ -20,43 +8,30 @@ def play(game):
     Args:
         game: certain game
     """
-    print(constants.INTRODUCTION)
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print('Hello, {user_name}!'.format(user_name=user_name))
+    print('{rule}'.format(rule=game.GAME_RULE))
 
-    user_name = prompt.string(
-        '{text} '.format(text=constants.PROMPT_FOR_NAME),
-    )
-    print('{text}, {user_name}!'.format(
-        text=constants.GREETING, user_name=user_name,
-    ))
-
-    print('{rules}'.format(rules=game.GAME_RULES))
-
-    for _ in range(0, constants.LOOPS_FOR_WIN):
+    loops_for_win = 3
+    for _ in range(0, loops_for_win):
         (question, correct_answer) = game.get_question_and_correct_answer()
-        print('{question}: {number}'.format(
-            question=constants.PROMPT_FOR_QUESTION,
-            number=question,
-        ))
-        user_answer = ask_user()
+        print('Question: {number}'.format(number=question))
+        user_answer = prompt.string('Your answer: ')
+
         if user_answer != correct_answer:
             print(
-                "'{user_answer}' {text}. ".format(
+                "'{user_answer}' is wrong answer ;(. ".format(
                     user_answer=user_answer,
-                    text=constants.CASE_INCORRECT_WRONG,
                 )
-                + "{text} '{correct_answer}'.\n".format(
-                    text=constants.CASE_INCORRECT_EXPECTED,
+                + "Correct answer was '{correct_answer}'.\n".format(
                     correct_answer=correct_answer,
                 )
-                + '{text}, {user_name}!'.format(
-                    text=constants.CASE_INCORRECT_REPEAT,
+                + "Let's try again, {user_name}!".format(
                     user_name=user_name,
                 ),
             )
-            break
-        print('{text}'.format(text=constants.CASE_CORRECT))
+            return
+        print('Correct!')
 
-    print('{text}, {user_name}!'.format(
-        text=constants.CONGRATULATION,
-        user_name=user_name,
-    ))
+    print('Congratulations, {user_name}!'.format(user_name=user_name))
