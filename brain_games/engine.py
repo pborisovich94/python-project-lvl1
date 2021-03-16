b2976f2552280b5cbf1f2d57aca170c84cf12fad
Prokhor Pastushenko <p.pastushenko@calltravel.eu>
2021-03-16 20:02:16 +0200

    replace "format" strings with "f" strings; change loops counter variable name

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -10,28 +10,22 @@ def play(game):
     """
     print('Welcome to the Brain Games!')
     user_name = prompt.string('May I have your name? ')
-    print('Hello, {user_name}!'.format(user_name=user_name))
-    print('{rule}'.format(rule=game.GAME_RULE))
+    print(f'Hello, {user_name}!')
+    print(game.GAME_RULE)
 
-    loops_for_win = 3
-    for _ in range(0, loops_for_win):
-        (question, correct_answer) = game.get_question_and_correct_answer()
-        print('Question: {number}'.format(number=question))
+    game_rounds_to_win = 3
+    for _ in range(0, game_rounds_to_win):
+        question, correct_answer = game.get_question_and_correct_answer()
+        print(f'Question: {question}')
         user_answer = prompt.string('Your answer: ')
 
         if user_answer != correct_answer:
             print(
-                "'{user_answer}' is wrong answer ;(. ".format(
-                    user_answer=user_answer,
-                )
-                + "Correct answer was '{correct_answer}'.\n".format(
-                    correct_answer=correct_answer,
-                )
-                + "Let's try again, {user_name}!".format(
-                    user_name=user_name,
-                ),
+                f"'{user_answer}' is wrong answer ;(. "
+                + f"Correct answer was '{correct_answer}'.\n"
+                + f"Let's try again, {user_name}!"
             )
             return
         print('Correct!')
 
-    print('Congratulations, {user_name}!'.format(user_name=user_name))
+    print(f'Congratulations, {user_name}!')
